62478ee7963f56d7a1a788bd59001cb2bafd16cd
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2023-04-20 19:33:43 -0600

    Sass configured

--- Back-end/client/src/App.js
+++ Back-end/client/src/App.js
@@ -1,4 +1,6 @@
  import './App.css';
+ //import './index.css';
+ import './styles.scss';
  import React from "react";
  import Header from './Components/Header';
 
--- Back-end/client/src/styles.js
+++ Back-end/client/src/styles.js
@@ -1,9 +1,9 @@
-import '../index.css';
+import './styles.scss';
 
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Mi Página Web en React con Sass</h1>
+      <h1 className="title">Mi Página Web en React con Sass hola</h1>
     </div>
   );
 }
