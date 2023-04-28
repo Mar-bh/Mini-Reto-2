d760c1a8d58760bcab8e92025cc6e16f8ff871cd
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2023-04-28 08:54:41 -0600

    test Index2.1

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,8 +1,31 @@
-const path = require('path');
+const express = require('express');
 
-const dirPath = '/var/log';
-const filePath = 'app.log';
+const bodyParser = require("body-parser");
 
-const fullPath = path.join(dirPath, filePath);
+const PORT = process.env.PORT || 3001;
 
-console.log(fullPath);
+const app = express();
+
+app.get('/api/hello', (req,res) => {
+    res.json({ message: "Hello world!" });
+});
+
+const fs = require("fs");
+
+app.get("/api/sound", async(req, res) => {
+    fs.readFile(__dirname + "/" + "sound.json", "utf8", (err, data) => {
+        console.log(data);
+        res.end(data);
+    });
+});
+
+app.use (bodyParser.json());
+
+app.post("/api/sound", (req, res) => {
+    console.log("El cuerpo de la peticion: ", req.body); 
+    res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
