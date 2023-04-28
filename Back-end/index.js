a41d23eaf8d6b4b54450168409fe84359e3de2eb
Mar Bustos <marianabhdez@gmail.com>
2023-04-28 10:59:56 -0600

    added endpoints to main

--- Back-end/index.js
+++ Back-end/index.js
@@ -30,6 +30,75 @@ app.get("/datos", (req, res) => {
   });
 });
 
+
+
+// GET
+app.get('/datos', (req, res) => {
+  connection.query('SELECT * FROM sounds', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.get('/playlist', (req, res) => {
+  connection.query('SELECT * FROM playlist_configuration', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+    console.log("Playlists: \t" + results);
+  });
+});
+
+app.get('/theme', (req,res) => {
+  connection.query("SELECT * FROM playlist JOIN playlist_sounds ON playlist.playlist_id = playlist_sounds.playlist_id"  , (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+    console.log("Playlists: \t" + results);
+  });
+})
+
+app.get('/theme/:id', (req,res) => {
+  connection.query("SELECT * FROM playlist JOIN playlist_sounds ON playlist.playlist_id = playlist_sounds.playlist_id WHERE playlist.playlist_id="+ req.params.id , (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+    console.log("Playlists: \t" + results);
+  });
+})
+
+app.get('/playlist/:id', (req, res) => {
+  connection.query('SELECT * FROM playlist_configuration WHERE id_configuration  = ' + req.params.id, (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+    console.log("Playlist: \t" + results + "Id: \t" + req.params.id);
+  });
+});
+
+// POST
+app.post('/playlist', (req, res) => {
+  connection.query('SELECT * FROM sounds', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+    console.log("Datos: \t" + results);
+  });
+});
+
+// PUT
+app.put('/playlist/:id', (req, res) => {
+  connection.query('SELECT * FROM sounds', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+    console.log("Datos: \t" + results);
+  });
+});
+
+// DELETE
+app.delete('/playlist/:id', (req, res) => {
+  connection.query('DELETE FROM playlist_configuration WHERE id_configuration = ' + req.params.id, (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+    console.log("Datos: \t" + results);
+  });
+});
+
 app.use(express.static(path.resolve("client/build")));
 // Todas las peticiones GET que no manejamos ahora regresarán nuestra React App
 // Agrega esto antes del “app.listen”
