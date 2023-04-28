91ff7868e45b25275160caab71c215b30790c3bf
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2023-04-28 08:38:05 -0600

    test Index1.5

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,38 +1,8 @@
-const express = require("express");
-const app = express();
-const filePath = require("cors");
-app.use(filePath());
+const path = require('path');
 
-require("dotenv").config();
-const mysql = require("mysql2");
+const dirPath = '/var/log';
+const filePath = 'app.log';
 
-const DB = process.env.DATABASE_URL;
-const connection = mysql.createConnection(DB);
-const PORT = process.env.PORT || 3001;
+const fullPath = path.join(dirPath, filePath);
 
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
-
-// GET
-app.get("/datos", (req, res) => {
-  connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    connection.query("SELECT * FROM sounds", (error, results, fields) => {
-      if (error) throw error;
-      res.send(results);
-    });
-  });
-});
-
-app.use(express.static(path.resolve("client/build")));
-// Todas las peticiones GET que no manejamos ahora regresarán nuestra React App
-// Agrega esto antes del “app.listen”
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("client/build", "index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+console.log(fullPath);
