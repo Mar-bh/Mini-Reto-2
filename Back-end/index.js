906086796f4a3f16b6ed2c0fe6572e8f2b64ffda
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2023-04-28 09:32:53 -0600

    basic index

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,8 +1,38 @@
-const path = require('path');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 
-const dirPath = '/var/log';
-const filePath = 'app.log';
+require("dotenv").config();
+const mysql = require("mysql2");
 
-const fullPath = path.join(dirPath, filePath);
+const DB = process.env.DATABASE_URL;
+const connection = mysql.createConnection(DB);
+const PORT = process.env.PORT || 3001;
 
-console.log(fullPath);
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+// GET
+app.get("/datos", (req, res) => {
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    connection.query("SELECT * FROM sounds", (error, results, fields) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+});
+
+app.use(express.static(path.resolve("client/build")));
+// Todas las peticiones GET que no manejamos ahora regresarán nuestra React App
+// Agrega esto antes del “app.listen”
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
