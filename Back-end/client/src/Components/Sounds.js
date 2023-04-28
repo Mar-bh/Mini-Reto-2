5230fec68737ce633ae0a04fd3c1336509314b1d
Mar Bustos <marianabhdez@gmail.com>
2023-04-28 14:54:48 -0600

    fetch to external api made, we were able to optain thhe preview sound

--- Back-end/client/src/Components/Sounds.js
+++ Back-end/client/src/Components/Sounds.js
@@ -2,30 +2,33 @@ import React, { useState } from "react";
 
 
 function Sounds() {
-  const [soundUrl, setSoundUrl] = useState("");
+  const [previewUrl, setPreviewUrl] = useState("");
   
   const getSoundPreview = async () => {
-    const soundId = "1234"; // Replace with the ID of the sound you want to preview
+    const soundId = "1234"; // Replace with the ID of the sound you want to get the preview for
     const apiKey = "49SUgyFMF5BUbPUEOpunCm4FsSmtCaasFuq0qm3i"; // Replace with your actual API key
-    const response = await fetch(
-      `https://freesound.org/apiv2/sounds/${soundId}/download/?token=${apiKey}`
-    );
-    const downloadUrl = await response.text();
-    setSoundUrl(downloadUrl);
+    const response = await fetch(`https://freesound.org/apiv2/sounds/${soundId}/`, {
+      headers: {
+        Authorization: `Token ${apiKey}`
+      }
+});
+    const data = await response.json();
+    console.log(data);
+    const previewUrl = data.previews["preview-hq-mp3"];
+    setPreviewUrl(previewUrl);
   };
 
   const playSound = () => {
-    const audio = new Audio(soundUrl);
+    console.log(previewUrl);
+    const audio = new Audio(previewUrl);
+    console.log(audio);
     audio.play();
   };
-
   return (
     <div>
-      <button onClick={getSoundPreview}>Get sound preview</button>
-      <button onClick={playSound} disabled={!soundUrl}>
-        Play sound
-      </button>
-    </div>
+    <button onClick={getSoundPreview}>Get sound preview</button>
+    <button onClick={playSound}>Play sound</button>
+  </div>
   );
 }
 
