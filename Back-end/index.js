c30a370b35fc2dce422ba55a58092fd484772ff9
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2023-04-28 09:04:36 -0600

    test vercel

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,31 +1,8 @@
-const express = require('express');
+const path = require('path');
 
-const bodyParser = require("body-parser");
+const dirPath = '/var/log';
+const filePath = 'app.log';
 
-const PORT = process.env.PORT || 3001;
+const fullPath = path.join(dirPath, filePath);
 
-const app = express();
-
-app.get('/api/hello', (req,res) => {
-    res.json({ message: "Hello world!" });
-});
-
-const fs = require("fs");
-
-app.get("/api/sound", async(req, res) => {
-    fs.readFile(__dirname + "/" + "sound.json", "utf8", (err, data) => {
-        console.log(data);
-        res.end(data);
-    });
-});
-
-app.use (bodyParser.json());
-
-app.post("/api/sound", (req, res) => {
-    console.log("El cuerpo de la peticion: ", req.body); 
-    res.sendStatus(200);
-});
-
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+console.log(fullPath);
