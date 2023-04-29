bdd270530adf90b6c5574813baea22146c4038f4
Mar Bustos <marianabhdez@gmail.com>
2023-04-29 14:36:56 -0600

    sound loops this time

--- Back-end/client/src/Components/Sounds.js
+++ Back-end/client/src/Components/Sounds.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 
 function Sounds() {
   const [previewUrl, setPreviewUrl] = useState("");
-  const [downloadUrl, setDownloadUrl] = useState("");
-  const [audio, setAudio] = useState(null); // Add state for the audio element	
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio, setAudio] = useState(null);
   
   const getSoundPreview = async () => {
     const soundId = "1234"; // Replace with the ID of the sound you want to get the preview for
@@ -17,34 +17,31 @@ function Sounds() {
     const data = await response.json();
     console.log(data);
     const previewUrl = data.previews["preview-hq-mp3"];
-    const downloadUrl = data.download;
-    console.log(downloadUrl);
+
     setPreviewUrl(previewUrl);
-    setDownloadUrl(downloadUrl);
   };
 
-  const playSound = () => {
-    console.log(previewUrl);
-    const newAudio = new Audio(previewUrl);
-    newAudio.loop = true;
-    console.log(newAudio);
-    setAudio(newAudio); // Save the audio element to state
-    newAudio.play();
-  };
-
-  const stopSound = () => {
-    if (audio) {
-      audio.pause(); // Pause the audio
-      audio.currentTime = 0; // Reset the playback position to the beginning
-      setAudio(null); // Remove the audio element from state
+  const handlePlay = () => {
+    if (!audio) {
+      const newAudio = new Audio(previewUrl);
+      newAudio.loop = true;
+      setAudio(newAudio);
+      setIsPlaying(true);
+      newAudio.play();
+    } else if (isPlaying) {
+      audio.pause();
+      audio.currentTime = 0;
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
     }
   };
 
   return (
     <div>
     <button onClick={getSoundPreview}>Get sound preview</button>
-    <button onClick={playSound}>Play sound</button>
-    <button onClick={stopSound}>Stop sound</button>
+    <button onClick={handlePlay}>{isPlaying ? 'Stop sound' : 'Play sound'}</button>
   </div>
   );
 }
