0a2ee95b864523e60173172fe74386db93c20215
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2023-04-29 15:26:23 -0600

    playlist base

--- Back-end/client/src/Components/LandingPage.jsx
+++ Back-end/client/src/Components/LandingPage.jsx
@@ -21,6 +21,10 @@ function Landing() {
   const [textbtn5, setTextbtn5] = useState('btn5');
   const [textbtn6, setTextbtn6] = useState('btn6');
 
+  const [showTheme, setShowTheme] = useState(true);
+  const [showPlaylist, setShowPlaylist] = useState(false);
+
+
   function handleClick(data) {
     setText(data);
   }
@@ -126,198 +130,278 @@ function Landing() {
           </div>
           <div className = "ThemeWindow2">
             <div className="container">
+
               <div className = "Options">
+
                 <button className="button"
                   onClick={() => {
-                      console.log("Datos: ", data);
-                    }}
+                    console.log("Datos: ", data);
+                    setShowPlaylist(false);
+                    setShowTheme(true);
+                  }}
                 >Theme</button>
                 <button className="button"
                   onClick={() => {
                       console.log("Datos: ", data);
+                      setShowTheme(false);
+                      setShowPlaylist(true);
                     }}
                 >Playlist</button>
               </div>
-  {/* ---------------------------------------------------------------- */}
-              <button className="button"
-                onClick={() => {
-                    temp = dataForest;
-                    handleClickbtn1(temp[0].sound_name);
-                    handleClickbtn2(temp[1].sound_name);
-                    handleClickbtn3(temp[2].sound_name);
-                    handleClickbtn4(temp[3].sound_name);
-                    handleClickbtn5(temp[4].sound_name);
-                    handleClickbtn6(temp[5].sound_name);
-                    console.log("Datos Bosque: ", dataForest);
-                    // console.log("FOREST");
-                    // console.log("TEMP: ", temp);
-                  }}
-              >Forest</button>
-
-              <button className="button"
-                onClick={() => {
-                    temp = dataSea;
-                    handleClickbtn1(temp[0].sound_name);
-                    handleClickbtn2(temp[1].sound_name);
-                    handleClickbtn3(temp[2].sound_name);
-                    handleClickbtn4(temp[3].sound_name);
-                    handleClickbtn5(temp[4].sound_name);
-                    handleClickbtn6(temp[5].sound_name);
-                    console.log("Datos Oceano: ", temp);
-                  }}
-              >Sea</button>
-
-              <button className="button"
-                onClick={() => {
-                    temp = dataLibrary;
-                    handleClickbtn1(temp[0].sound_name);
-                    handleClickbtn2(temp[1].sound_name);
-                    handleClickbtn3(temp[2].sound_name);
-                    handleClickbtn4(temp[3].sound_name);
-                    handleClickbtn5(temp[4].sound_name);
-                    handleClickbtn6(temp[5].sound_name);
-                    console.log("Datos Biblio: ", temp);
-                  }}
-              >Library</button>
-
-              <button className="button"
-                onClick={() => {
-                  temp = dataCoffee;
-                  handleClickbtn1(temp[0].sound_name);
-                  handleClickbtn2(temp[1].sound_name);
-                  handleClickbtn3(temp[2].sound_name);
-                  handleClickbtn4(temp[3].sound_name);
-                  handleClickbtn5(temp[4].sound_name);
-                  handleClickbtn6(temp[5].sound_name);
-                  console.log("Datos Cafe: ", temp);
-                }}
-              >Coffee Shop</button>
-            </div>
-
-
-            <div className = "volume">
-              <div className = "col">
-                <button className="sound"onClick={() => {
-                        console.log("Volumen: ");
+  
+              {showTheme && (
+                <div className = "Themes">     
+                  <button className="button"
+                    onClick={() => {
+                        temp = dataForest;
+                        handleClickbtn1(temp[0].sound_name);
+                        handleClickbtn2(temp[1].sound_name);
+                        handleClickbtn3(temp[2].sound_name);
+                        handleClickbtn4(temp[3].sound_name);
+                        handleClickbtn5(temp[4].sound_name);
+                        handleClickbtn6(temp[5].sound_name);
+                        console.log("Datos Bosque: ", dataForest);
+                        // console.log("FOREST");
+                        // console.log("TEMP: ", temp);
                       }}
-                >V</button>
+                  >Forest</button>
 
-                {/* FOREST */}
-                <button className="buttonSound"
-                  onClick={() => {
-                      // temp = dataForest;
-                      console.log(temp[0].sound_name + "sounds", temp[0]);
-                      handleClick(temp[0].sound_name);
-                      // handleClickbtn1(temp[0].sound_name);
+                  <button className="button"
+                    onClick={() => {
+                        temp = dataSea;
+                        handleClickbtn1(temp[0].sound_name);
+                        handleClickbtn2(temp[1].sound_name);
+                        handleClickbtn3(temp[2].sound_name);
+                        handleClickbtn4(temp[3].sound_name);
+                        handleClickbtn5(temp[4].sound_name);
+                        handleClickbtn6(temp[5].sound_name);
+                        console.log("Datos Oceano: ", temp);
+                      }}
+                  >Sea</button>
+
+                  <button className="button"
+                    onClick={() => {
+                        temp = dataLibrary;
+                        handleClickbtn1(temp[0].sound_name);
+                        handleClickbtn2(temp[1].sound_name);
+                        handleClickbtn3(temp[2].sound_name);
+                        handleClickbtn4(temp[3].sound_name);
+                        handleClickbtn5(temp[4].sound_name);
+                        handleClickbtn6(temp[5].sound_name);
+                        console.log("Datos Biblio: ", temp);
+                      }}
+                  >Library</button>
+
+                  <button className="button"
+                    onClick={() => {
+                      temp = dataCoffee;
+                      handleClickbtn1(temp[0].sound_name);
+                      handleClickbtn2(temp[1].sound_name);
+                      handleClickbtn3(temp[2].sound_name);
+                      handleClickbtn4(temp[3].sound_name);
+                      handleClickbtn5(temp[4].sound_name);
+                      handleClickbtn6(temp[5].sound_name);
+                      console.log("Datos Cafe: ", temp);
                     }}
-                    // >{1 ? <p>Sound name: {temp[0].sound_name}</p> : <p>No sound name available</p>}</button>
-                    // >1</button>
-                  >{textbtn1}</button> 
+                  >Coffee Shop</button>
+                </div>
+              )}
 
-                <button className="play"onClick={() => {
-                        console.log("Play: ");
+              {showPlaylist && (
+                <div className = "Playlist">     
+                  <button className="buttonPlaylist"
+                    onClick={() => {
+                        temp = dataForest;
+                        handleClickbtn1(temp[0].sound_name);
+                        handleClickbtn2(temp[1].sound_name);
+                        handleClickbtn3(temp[2].sound_name);
+                        handleClickbtn4(temp[3].sound_name);
+                        handleClickbtn5(temp[4].sound_name);
+                        handleClickbtn6(temp[5].sound_name);
+                        console.log("Datos Bosque: ", dataForest);
                       }}
-                >II</button>
-                <button className="sound"onClick={() => {
-                        console.log("Volumen: ");
-                      }}
-                >V</button>
+                  >Monday</button>
 
-                <button className="buttonSound"
-                  onClick={() => {
-                      // temp = dataForest;
-                      console.log(temp[1].sound_name + "sounds", temp[1]);
-                      handleClick(temp[1].sound_name);
-                      // handleClickbtn2(temp[1].sound_name);
+                  <button className="buttonPlaylist"
+                    onClick={() => {
+                        temp = dataSea;
+                        handleClickbtn1(temp[0].sound_name);
+                        handleClickbtn2(temp[1].sound_name);
+                        handleClickbtn3(temp[2].sound_name);
+                        handleClickbtn4(temp[3].sound_name);
+                        handleClickbtn5(temp[4].sound_name);
+                        handleClickbtn6(temp[5].sound_name);
+                        console.log("Datos Oceano: ", temp);
+                      }}
+                  >Tuesday</button>
+
+                  <button className="buttonPlaylist"
+                    onClick={() => {
+                        temp = dataLibrary;
+                        handleClickbtn1(temp[0].sound_name);
+                        handleClickbtn2(temp[1].sound_name);
+                        handleClickbtn3(temp[2].sound_name);
+                        handleClickbtn4(temp[3].sound_name);
+                        handleClickbtn5(temp[4].sound_name);
+                        handleClickbtn6(temp[5].sound_name);
+                        console.log("Datos Biblio: ", temp);
+                      }}
+                  >Wednesday</button>
+
+                  <button className="buttonPlaylist"
+                    onClick={() => {
+                      temp = dataCoffee;
+                      handleClickbtn1(temp[0].sound_name);
+                      handleClickbtn2(temp[1].sound_name);
+                      handleClickbtn3(temp[2].sound_name);
+                      handleClickbtn4(temp[3].sound_name);
+                      handleClickbtn5(temp[4].sound_name);
+                      handleClickbtn6(temp[5].sound_name);
+                      console.log("Datos Cafe: ", temp);
                     }}
-                // >2</button>
-                >{textbtn2}</button>
+                  >Thursday</button>
 
-                <button className="play"onClick={() => {
-                        console.log("Play: ");
-                      }}
-                >II</button>
+                  <button className="buttonPlaylist"
+                    onClick={() => {
+                      temp = dataCoffee;
+                      handleClickbtn1(temp[0].sound_name);
+                      handleClickbtn2(temp[1].sound_name);
+                      handleClickbtn3(temp[2].sound_name);
+                      handleClickbtn4(temp[3].sound_name);
+                      handleClickbtn5(temp[4].sound_name);
+                      handleClickbtn6(temp[5].sound_name);
+                      console.log("Datos Cafe: ", temp);
+                    }}
+                  >Friday</button>
+
+                </div>
+              )}
+
+              <div className = "volume">
+                <div className = "col">
+                    <button className="sound"onClick={() => {
+                            console.log("Volumen: ");
+                          }}
+                    >V</button>
+
+                    {/* FOREST */}
+                    <button className="buttonSound"
+                      onClick={() => {
+                          // temp = dataForest;
+                          console.log(temp[0].sound_name + "sounds", temp[0]);
+                          handleClick(temp[0].sound_name);
+                          // handleClickbtn1(temp[0].sound_name);
+                        }}
+                        // >{1 ? <p>Sound name: {temp[0].sound_name}</p> : <p>No sound name available</p>}</button>
+                        // >1</button>
+                      >{textbtn1}</button> 
+
+                    <button className="play"onClick={() => {
+                            console.log("Play: ");
+                          }}
+                    >II</button>
+                    <button className="sound"onClick={() => {
+                            console.log("Volumen: ");
+                          }}
+                    >V</button>
+
+                    <button className="buttonSound"
+                      onClick={() => {
+                          // temp = dataForest;
+                          console.log(temp[1].sound_name + "sounds", temp[1]);
+                          handleClick(temp[1].sound_name);
+                          // handleClickbtn2(temp[1].sound_name);
+                        }}
+                    // >2</button>
+                    >{textbtn2}</button>
+
+                    <button className="play"onClick={() => {
+                            console.log("Play: ");
+                          }}
+                    >II</button>
+                </div>
+                <div className = "col">
+                    <button className="sound"onClick={() => {
+                            console.log("Volumen: ");
+                          }}
+                    >V</button>
+
+                    <button className="buttonSound"
+                      onClick={() => {
+                          // temp = dataForest;
+                          console.log(temp[2].sound_name + "sounds", temp[2]);
+                          handleClick(temp[2].sound_name);
+                          // handleClickbtn3(temp[2].sound_name);
+                        }}
+                    // >3</button>
+                    >{textbtn3}</button>
+
+                    <button className="play"onClick={() => {
+                            console.log("Play: ");
+                          }}
+                    >II</button>
+                    <button className="sound"onClick={() => {
+                            console.log("Volumen: ");
+                          }}
+                    >V</button>
+                    
+                    <button className="buttonSound"
+                      onClick={() => {
+                          // temp = dataForest;
+                          console.log(temp[3].sound_name + "sounds", temp[3]);
+                          handleClick(temp[3].sound_name);
+                          // handleClickbtn4(temp[3].sound_name);
+                        }}
+                    // >4</button>
+                    >{textbtn4}</button>
+
+                    <button className="play"onClick={() => {
+                            console.log("Play: ");
+                          }}
+                    >II</button>
+                </div>
+                <div className = "col">
+                    <button className="sound"onClick={() => {
+                            console.log("Volumen: ");
+                          }}
+                    >V</button>
+
+                    <button className="buttonSound"
+                      onClick={() => {
+                          // temp = dataForest;
+                          console.log(temp[4].sound_name + "sounds", temp[4]);
+                          handleClick(temp[4].sound_name);
+                          // handleClickbtn5(temp[4].sound_name);
+                        }}
+                    // >5</button>
+                    >{textbtn5}</button>
+
+                    <button className="play"onClick={() => {
+                            console.log("Play: ");
+                          }}
+                    >II</button>
+                    <button className="sound"onClick={() => {
+                            console.log("Volumen: ");
+                          }}
+                    >V</button>
+
+                    <button className="buttonSound"
+                      onClick={() => {
+                          // temp = dataForest;
+                          console.log(temp[5].sound_name + "sounds", temp[5]);
+                          handleClick(temp[5].sound_name);
+                          // handleClickbtn6(temp[5].sound_name);
+                        }}
+                    // >6</button>
+                    >{textbtn6}</button>
+
+                    <button className="play"onClick={() => {
+                            console.log("Play: ");
+                          }}
+                    >II</button>
+                </div>
               </div>
-              <div className = "col">
-                <button className="sound"onClick={() => {
-                        console.log("Volumen: ");
-                      }}
-                >V</button>
 
-                <button className="buttonSound"
-                  onClick={() => {
-                      // temp = dataForest;
-                      console.log(temp[2].sound_name + "sounds", temp[2]);
-                      handleClick(temp[2].sound_name);
-                      // handleClickbtn3(temp[2].sound_name);
-                    }}
-                // >3</button>
-                >{textbtn3}</button>
-
-                <button className="play"onClick={() => {
-                        console.log("Play: ");
-                      }}
-                >II</button>
-                <button className="sound"onClick={() => {
-                        console.log("Volumen: ");
-                      }}
-                >V</button>
-                
-                <button className="buttonSound"
-                  onClick={() => {
-                      // temp = dataForest;
-                      console.log(temp[3].sound_name + "sounds", temp[3]);
-                      handleClick(temp[3].sound_name);
-                      // handleClickbtn4(temp[3].sound_name);
-                    }}
-                // >4</button>
-                >{textbtn4}</button>
-
-                <button className="play"onClick={() => {
-                        console.log("Play: ");
-                      }}
-                >II</button>
-              </div>
-              <div className = "col">
-                <button className="sound"onClick={() => {
-                        console.log("Volumen: ");
-                      }}
-                >V</button>
-
-                <button className="buttonSound"
-                  onClick={() => {
-                      // temp = dataForest;
-                      console.log(temp[4].sound_name + "sounds", temp[4]);
-                      handleClick(temp[4].sound_name);
-                      // handleClickbtn5(temp[4].sound_name);
-                    }}
-                // >5</button>
-                >{textbtn5}</button>
-
-                <button className="play"onClick={() => {
-                        console.log("Play: ");
-                      }}
-                >II</button>
-                <button className="sound"onClick={() => {
-                        console.log("Volumen: ");
-                      }}
-                >V</button>
-
-                <button className="buttonSound"
-                  onClick={() => {
-                      // temp = dataForest;
-                      console.log(temp[5].sound_name + "sounds", temp[5]);
-                      handleClick(temp[5].sound_name);
-                      // handleClickbtn6(temp[5].sound_name);
-                    }}
-                // >6</button>
-                >{textbtn6}</button>
-
-                <button className="play"onClick={() => {
-                        console.log("Play: ");
-                      }}
-                >II</button>
-              </div>
                 <br/>
                 <p>Output: {text}</p>
             </div>   
